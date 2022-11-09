9f17132bf608c9323114a6e3052e1b429c725237
Ryland Adams <rylandadams@yahoo.com>
2022-11-09 13:57:45 -0800

    general concept

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -15,6 +15,14 @@ if answer == "left":
     else:
         print('not a valid option. you lose.')
 elif answer == "right":
-    print()
+    answer = input(
+        "there is a forest, do you want to go in or around?").lower()
+    if answer == "in":
+        print("you wander around and get lost in the dark")
+    elif answer == "around":
+        print("you walk for days and days... there is no way around")
+    else:
+        print('not a valid option. you lose.')
+
 else:
     print("not a valid option")
